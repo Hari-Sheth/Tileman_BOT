76ff0a11dd7fc73b18d101980ced1f34ac6d78df
Aaryan Chavda <aaryanchavda0109@gmail.com>
2026-06-12 17:38:41 +0530

    Add files via upload
    Added Selenium.py which contains code for initializing the tileman bot

---
+++ SeleniumTrial.py
@@ -0,0 +1,35 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+#Initialising driver
+
+driver = webdriver.Chrome()
+driver.get("https://tileman.io/")
+
+body_element = driver.find_element(By.TAG_NAME, "body")
+nickname_element = driver.find_element(By.ID, "nick")
+
+# setting nickname to assert dominance
+
+nickname_element.clear()
+nickname_element.send_keys("tldr")
+
+# starting the game
+
+body_element.send_keys(Keys.ENTER)
+
+# waiting 2 sec buffer (hopefully no ads idk)
+
+time.sleep(2)
+
+# control start from here
+
+body_element.send_keys(Keys.ARROW_UP)
+
+time.sleep(10)
+
+# quit driver to close
+
+driver.quit()
