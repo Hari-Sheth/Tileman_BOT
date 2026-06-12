cd0aa4c67a2476a1468d19f5169e4052197bbb58
Daksh-Maahor <daksh.dav2007@gmail.com>
2026-06-12 17:54:58 +0530

    Added a bot class to make testing easier

--- SeleniumTrial.py
+++ SeleniumTrial.py
@@ -7,20 +7,44 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 
+class Bot:
+    def __init__(self, name, body_element):
+        self.name = name
+        self.controller = body_element
+
+    def up(self):
+        self.controller.send_keys(Keys.ARROW_UP)
+
+    def down(self):
+        self.controller.send_keys(Keys.ARROW_DOWN)
+
+    def left(self):
+        self.controller.send_keys(Keys.ARROW_LEFT)
+
+    def right(self):
+        self.controller.send_keys(Keys.ARROW_RIGHT)
+
+    def run():
+        pass
+
 try:
     driver.get("https://tileman.io/")
 
     body_element = driver.find_element(By.TAG_NAME, "body")
     nickname_element = driver.find_element(By.ID, "nick")
 
+    nickname = "tldr"
+
     # setting nickname to assert dominance
 
     nickname_element.clear()
-    nickname_element.send_keys("tldr")
+    nickname_element.send_keys(nickname)
 
     # starting the game
 
     body_element.send_keys(Keys.ENTER)
+
+    bot = Bot(nickname, body_element)
 
     # waiting 2 sec buffer (hopefully no ads idk)
 
@@ -28,7 +52,7 @@ try:
 
     # control start from here
 
-    body_element.send_keys(Keys.ARROW_UP)
+    bot.run()
 
     time.sleep(10)
 finally:
