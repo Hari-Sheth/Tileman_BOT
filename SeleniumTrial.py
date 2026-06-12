200a29d9c917c864e59e5be9c21e4b956f709eb0
Daksh-Maahor <daksh.dav2007@gmail.com>
2026-06-13 00:54:13 +0530

    Added a general bot class and a random bot
    Managed to get a good score from random bot somehow :)
    
    Also very unoptimized code for now

--- SeleniumTrial.py
+++ SeleniumTrial.py
@@ -2,15 +2,17 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import random
 
 #Initialising driver
 
 driver = webdriver.Chrome()
 
 class Bot:
-    def __init__(self, name, body_element):
+    def __init__(self, name, body_element, overlay_element):
         self.name = name
         self.controller = body_element
+        self.overlay = overlay_element
 
     def up(self):
         self.controller.send_keys(Keys.ARROW_UP)
@@ -24,16 +26,40 @@ class Bot:
     def right(self):
         self.controller.send_keys(Keys.ARROW_RIGHT)
 
-    def run():
+    def check_lose(self):
+        return self.overlay.get_dom_attribute("style") == "display: block;"
+
+    def run(self):
         pass
+
+class RandomBot(Bot):
+    def __init__(self, name, body_element, overlay_element):
+        super().__init__(name, body_element, overlay_element)
+    
+    def run(self):
+        while True:
+            c = random.randint(1, 4)
+
+            if c == 1:
+                self.up()
+            elif c == 2:
+                self.down()
+            elif c == 3:
+                self.left()
+            else:
+                self.right()
+
+            if self.check_lose():
+                self.controller.send_keys(Keys.RETURN)
+                self.controller.send_keys(Keys.RETURN)
 
 try:
     driver.get("https://tileman.io/")
 
     body_element = driver.find_element(By.TAG_NAME, "body")
+    overlay_element = driver.find_element(By.ID, "overlay")
     nickname_element = driver.find_element(By.ID, "nick")
-
-    nickname = "tldr"
+    nickname = "Bhiku"
 
     # setting nickname to assert dominance
 
@@ -44,17 +70,16 @@ try:
 
     body_element.send_keys(Keys.ENTER)
 
-    bot = Bot(nickname, body_element)
+    bot = RandomBot(nickname, body_element, overlay_element)
 
     # waiting 2 sec buffer (hopefully no ads idk)
 
     time.sleep(2)
 
     # control start from here
-
     bot.run()
 
-    time.sleep(10)
+    time.sleep(5)
 finally:
     # quit driver to close
 
